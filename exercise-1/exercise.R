c912511ca1c93478a3fd5c07c4d6d2a32064dbd8
Stephen Hung <hung.stephen.yh@gmail.com>
2017-01-10 14:45:09 -0800

    finished exercise-1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -2,23 +2,32 @@
 
 # Create a variable `hometown` that stores the city in which you were born
 
+hometown <- "Boise"
 
 # Assign your name to the variable `my.name`
 
+my.name <- "Stephen"
 
 # Assign your height (in inches) to a variable `my.height`
 
+my.height <- 73
 
 # Create a variable `puppies` equal to the number of puppies you'd like to have
 
+puppies <- 100
 
 # Create a variable `puppy.price`, which is how expensive you think a puppy is
 
+puppy.price <- 2
 
 # Create a variable `total.cost` that has the total cost of all of your puppies
 
+total.cost <- puppies * puppy.price
 
 # Create a boolean variable `too.expensive`, set to TRUE if the cost is greater than $1,000
 
+too.expensive <- total.cost > 1000
 
 # Create a variable `max.puppies`, which is the number of puppies you can afford for $1,000.
+
+max.puppies <- 1000/puppy.price
